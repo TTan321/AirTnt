bb9041934ea4ca0a3ce15580cad6d680f4e8ecf9
TonyTan <tonytan2193@gmail.com>
2022-08-02 21:03:59 -0700

    changed dates to null in booking seed

--- backend/db/seeders/20220802234046-demo-booking.js
+++ backend/db/seeders/20220802234046-demo-booking.js
@@ -6,20 +6,14 @@ module.exports = {
       {
         spotId: 1,
         userId: 3,
-        startDate: 2022 - 12 - 21,
-        endDate: 2022 - 12 - 31
       },
       {
         spotId: 2,
         userId: 1,
-        startDate: 2022 - 10 - 25,
-        endDate: 2022 - 10 - 31
       },
       {
         spotId: 3,
         userId: 2,
-        startDate: 2022 - 11 - 21,
-        endDate: 2022 - 11 - 31
       },
     ])
   },
