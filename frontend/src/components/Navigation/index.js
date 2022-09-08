0e4cac87ae7c2f615ce40b30351cda65e915c4ec
TonyTan <tonytan2193@gmail.com>
2022-09-08 00:11:01 -0700

    added airtnt logo to web page title and home button

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import LoginFormModal from '../LoginFormModal/LoginModal';
 import SignUpFormModel from '../SignUpFormModal/SignUpFormModel';
+import airtnt from '../../images/airtnt-logo.png';
 import './Navigation.css';
 
 function Navigation({ isLoaded }) {
@@ -25,7 +26,7 @@ function Navigation({ isLoaded }) {
 
     return (
         <div className='navBar'>
-            <NavLink exact to="/" className={"nav-links home"} >Home</NavLink>
+            <NavLink exact to="/" className={"nav-links home"} ><img src={airtnt} alt="Home" /></NavLink>
             <div className='nav-links'>
                 <NavLink exact to="/hostspot" className={"nav-links host-link"}>Become a Host</NavLink>
                 {isLoaded && sessionLinks}
