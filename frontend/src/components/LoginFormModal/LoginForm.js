bdbf2fb00f92be2937cd4b16957e7ccd2ae6d235
TonyTan <tonytan2193@gmail.com>
2022-09-07 22:55:56 -0700

    added red color to login error messages

--- frontend/src/components/LoginFormModal/LoginForm.js
+++ frontend/src/components/LoginFormModal/LoginForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import * as sessionActions from "../../store/session";
 import { useDispatch } from "react-redux";
+import xfavicon from '../../images/X-favicon.ico'
 import './LoginModal.css';
 
 function LoginForm() {
@@ -20,6 +21,10 @@ function LoginForm() {
         );
     };
 
+    if (!!errors.length) {
+
+    }
+
     const logInDemo = (e) => {
         e.preventDefault();
 
@@ -29,6 +34,7 @@ function LoginForm() {
     return (
         <>
             <div className="header-div">
+                <button className="cancel-button" ><img src={xfavicon} alt="cancel" /></button>
                 <h1 className="h1">Log In</h1>
             </div>
             <form className="form" onSubmit={handleSubmit}>
@@ -49,11 +55,13 @@ function LoginForm() {
                     placeholder="Password"
                     required
                 />
-                <ul>
-                    {errors.map((error, idx) => (
-                        <li key={idx}>{error}</li>
-                    ))}
-                </ul>
+                {!!errors.length && (
+                    <div className="login-errors-container">
+                        {errors.map((error, idx) => (
+                            <p className="login-errors" key={idx}>! {error}</p>
+                        ))}
+                    </div>
+                )}
                 <button className="submit" type="submit">Log In</button>
                 <button className="demo-user" onClick={logInDemo}>Demo User</button>
             </form>
