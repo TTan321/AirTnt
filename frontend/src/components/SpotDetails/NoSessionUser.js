104e08d4a6886e09f789de20f7527aa84ce6edde
TonyTan <tonytan2193@gmail.com>
2022-09-14 14:18:31 -0700

    finished styling no logged in user spot details page with reviews

--- frontend/src/components/SpotDetails/NoSessionUser.js
+++ frontend/src/components/SpotDetails/NoSessionUser.js
@@ -13,6 +13,7 @@ function NoUserSpotDetails() {
     const allReviews = useSelector((state) => (state.reviews))
     const reviews = Object.values(allReviews);
 
+    console.log(spot)
     console.log(reviews)
 
     useEffect(() => {
@@ -27,7 +28,10 @@ function NoUserSpotDetails() {
                     <div className="spot">
                         <h1 className='header'>{spot.name}</h1>
                         <div className="description-container">
-                            <p className="d1"><span className="all-spots-star"><i className="fas fa-star" /> </span> {!!spot.avgRating ? spot.avgRating.toFixed(2) : "0"} - {reviews.length} Reviews</p>
+                            <p className="d1">
+                                <span className="all-spots-star"><i className="fas fa-star" /> </span>
+                                {!!spot.avgRating ? spot.avgRating.toFixed(2) : "0"} - {reviews.length} Reviews
+                            </p>
                             <p className="d2">{spot.city}, {spot.state}, {spot.country} </p>
                         </div>
                         <div className='image-container'>
@@ -43,23 +47,61 @@ function NoUserSpotDetails() {
                         </div>
                         <div className='bottom-container'>
                             <div className='spot-description'>
-                                <p>{spot.description}</p>
+                                <p>{spot.description} by { }</p>
                             </div>
                             <div className='bookings'>
-                                <p className="price"><span className="price">${spot.price}</span> night</p>
+                                <div className='booking-description'>
+                                    <p className="price"><span className="price">${spot.price}</span> night</p>
+                                    <p className="d1">
+                                        <span className="all-spots-star"><i className="fas fa-star" /> </span>
+                                        {!!spot.avgRating ? spot.avgRating.toFixed(2) : "0"} - {reviews.length} Reviews
+                                    </p>
+                                </div>
+                                <div className='calculation'>
+                                    <div className='left'>
+                                        <p className='b-p'> ${spot.price} x 5 nights</p>
+                                        <p className='b-p'>Cleaning Fee</p>
+                                        <p className='b-p'>Service Fee</p>
+                                    </div>
+                                    <div className='right'>
+                                        <p className='b-p'> ${spot.price * 5} </p>
+                                        <p className='b-p'>$150</p>
+                                        <p className='b-p'>$260</p>
+                                    </div>
+                                </div>
+                                <div className='total'>
+                                    <div>
+                                        <p>Total before taxes</p>
+                                    </div>
+                                    <div>
+                                        <p>${spot.price * 5 + 150 + 260}</p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         <div className='reviews-container'>
-                            {reviews.map(review => (
-                                <div className='review' key={review.id} >
-                                    <div className='review-user'>
-                                        <p>{review.User.firstName} {review.User.lastName}</p>
+                            <h3 className='reviews-header'>
+                                <span className="all-spots-star"><i className="fas fa-star" /> </span>
+                                {!!spot.avgRating ? spot.avgRating.toFixed(2) : "0"} - {reviews.length} Reviews
+                            </h3>
+                            <div className='review-grid'>
+                                {reviews.map(review => (
+                                    <div className='review' key={review.id} >
+                                        <div className='review-user'>
+                                            <div className='profile'>
+                                                <i class="fas fa-user-circle fa-2x" />
+                                            </div>
+                                            <div className='user-name'>
+                                                <p className='b-p'>{review.User.firstName} {review.User.lastName}</p>
+                                                <p className='b-p'>{review.createdAt.slice(0, 10)}</p>
+                                            </div>
+                                        </div>
+                                        <div className='review-description'>
+                                            <p>{review.review}</p>
+                                        </div>
                                     </div>
-                                    <div className='review-description'>
-                                        <p>{review.review}</p>
-                                    </div>
-                                </div>
-                            ))}
+                                ))}
+                            </div>
                         </div>
                     </div>
                 </div>
--- frontend/src/components/Spots/spots.js
+++ frontend/src/components/Spots/spots.js
@@ -2,23 +2,20 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 import './spots.css';
 
-function Spots({ spots, user }) {
+function Spots({ spots }) {
     const history = useHistory();
-
-    console.log(user)
-
-    const redirect = (id) => {
-        history.push(`/spots/${id}`)
-    }
 
     return (
         <div className="all-spots-container">
             {spots.map(({ id, previewImage, city, state, avgRating, price }) => (
-                <div onClick={() => redirect(id)} key={id} className="spot-container">
+                <div onClick={() => history.push(`/spots/${id}`)} key={id} className="spot-container">
                     <img src={previewImage} alt={"Preview Spot"} className="images" />
                     <div className="spotsDescription">
                         <p className="allSpotsP1">{city}, {state}</p>
-                        <p className="allSpotsP2"><span className="all-spots-star"><i className="fas fa-star" /> </span> {!!avgRating ? avgRating.toFixed(2) : "0"} </p>
+                        <p className="allSpotsP2">
+                            <span className="all-spots-star"><i className="fas fa-star" />
+                            </span> {!!avgRating ? avgRating.toFixed(2) : "0"}
+                        </p>
                     </div>
                     <p className="allSpotsP3"><span className="price">${price}</span> night</p>
                 </div>
