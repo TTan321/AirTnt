8ae0b9f1f42285068e2d806228c29cd690b00ded
TonyTan <tonytan2193@gmail.com>
2023-01-10 03:00:58 -0800

    Styled edit booking form

--- frontend/src/components/UserBookings/EditBooking.js
+++ frontend/src/components/UserBookings/EditBooking.js
@@ -2,6 +2,7 @@ import { useState } from "react"
 import { useDispatch } from "react-redux"
 import { loadBookings, updateBooking } from "../../store/bookings"
 import { loadUsersBookings } from "../../store/sessionBooking"
+import "./EditBooking.css"
 
 function EditBooking({ booking, setShowModal }) {
     const dispatch = useDispatch();
@@ -36,33 +37,43 @@ function EditBooking({ booking, setShowModal }) {
         await dispatch(loadBookings())
     }
 
+    let nights = ((new Date(checkOut) - new Date(checkIn)) / 86400000)
+
     return (
-        <form onSubmit={(e) => onSubmit(e)}>
-            <div>
-                <label>
-                    Check in
+        <form onSubmit={(e) => onSubmit(e)} className="editBooking">
+            <div style={{ fontFamily: "sans-serif", fontWeight: "bold", fontSize: "22px", borderBottom: "1px solid black", marginBottom: "10px" }}>Edit your reservation</div>
+            <div>{booking.Spot.name}</div>
+            <p className="l-p"><span className="edit-location">{booking.Spot.city}, {booking.Spot.state ? booking.Spot.state : booking.Spot.country} </span></p>
+            <p className="l-p"><span className="listing-price">Reservation Cost: ${booking.Spot.price * nights} </span></p>
+            <div className='calendar'>
+                <div className="CheckIn">
+                    <span style={{ paddingRight: '20px' }}>
+                        Check In:
+                    </span>
                     <input
                         type={"date"}
                         value={checkIn}
                         min={`${year}-${month}-${date}`}
                         onChange={(e) => setCheckIn(e.target.value)}
                     />
-                </label>
-            </div>
-            <div>
-                <label>
-                    Check out
+                </div>
+                <div className="CheckOut">
+                    <span style={{ paddingRight: '9px' }}>
+                        Check Out:
+                    </span>
                     <input
                         type={"date"}
                         value={checkOut}
                         min={`${year}-${month}-${date}`}
                         onChange={(e) => setCheckOut(e.target.value)}
                     />
-                </label>
+                </div>
             </div>
-            <button type="submit" >Submit</button>
-            <button onClick={() => setShowModal(false)}>Cancel</button>
-        </form>
+            <div className="editBookButton">
+                <button type="submit" className="edit" >Submit</button>
+                <button onClick={() => setShowModal(false)} className="edit" >Cancel</button>
+            </div>
+        </form >
     )
 }
 
--- frontend/src/components/UserBookings/UserBookings.js
+++ frontend/src/components/UserBookings/UserBookings.js
@@ -18,7 +18,7 @@ function UserBookings() {
 
     return bookingsArr.length > 0 && (
         <div className="user-spots-page-container">
-            <h1 style={{ paddingTop: "10px" }}>User BOOKINGS</h1>
+            <h1 style={{ padding: "10px 0", fontFamily: "sans-serif", borderBottom: "1px solid black" }}>Your Reservations</h1>
             <div className="user-spots-container">
                 {bookingsArr.map(booking => (
                     <div key={booking.id} className="listings-spot-container">
@@ -28,7 +28,7 @@ function UserBookings() {
                             </p>
                             <img src={booking.Spot.previewImage} alt={"Spot"} className="listing-images" />
                             <p className="l-p"><span className="edit-location">{booking.Spot.city}, {booking.Spot.state ? booking.Spot.state : booking.Spot.country} </span></p>
-                            <p className="l-p"><span className="listing-price">${booking.Spot.price} </span>a night </p>
+                            <p className="l-p"><span className="listing-price">Reservation Cost: ${booking.Spot.price * ((new Date(booking.endDate) - new Date(booking.startDate)) / 86400000)} </span></p>
                             <div style={{ paddingTop: "10px", display: "flex", flexDirection: "column", fontSize: "14px" }}>
                                 <span>
                                     <span style={{ fontWeight: "bold" }}>Check in:</span> {booking.startDate.slice(5, -14)}-{booking.startDate.slice(0, 4)}
