1a19b3b34f6623b387f5cccb3bdf85035f27288a
TonyTan <tonytan2193@gmail.com>
2023-02-22 16:24:00 -0800

    Updated users bookings. New bookings show first, cannot edit past bookings

--- frontend/src/components/UserBookings/EditBookingModal.js
+++ frontend/src/components/UserBookings/EditBookingModal.js
@@ -6,9 +6,21 @@ import EditBooking from './EditBooking';
 function EditBookingModal({ id, booking }) {
     const [showModal, setShowModal] = useState(false);
 
+    const openForm = () => {
+        let currentDate = new Date()
+        let checkOut = `${booking.endDate.slice(0, 4)}-${booking.endDate.slice(5, 7)}-${booking.endDate.slice(8, 10)}`
+        if (new Date(checkOut) < currentDate) {
+            alert("Cannot edit past bookings")
+            setShowModal(false)
+            return
+        } else {
+            setShowModal(true)
+        }
+    }
+
     return (
         <>
-            <button className='edit' onClick={() => setShowModal(true)}>Edit</button>
+            <button className='edit' onClick={openForm}>Edit</button>
             {showModal && (
                 <Modal onClose={() => setShowModal(false)}>
                     <EditBooking setShowModal={setShowModal} id={id} booking={booking} />
