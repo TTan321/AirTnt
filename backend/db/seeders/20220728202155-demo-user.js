fd2004918859dfac1789f548c8279194822fd010
Tony Tan <104934632+TTan321@users.noreply.github.com>
2025-07-20 19:13:35 -0700

    Update 20220728202155-demo-user.js

--- backend/db/seeders/20220728202155-demo-user.js
+++ backend/db/seeders/20220728202155-demo-user.js
@@ -50,7 +50,7 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    options.tablename = 'Bookings'
+    options.tablename = 'Users'
     return queryInterface.bulkDelete(options, {});
   }
 };
