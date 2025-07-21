a2d5b036123310763299a08a66f9afb228da3ac6
Tony Tan <104934632+TTan321@users.noreply.github.com>
2025-07-20 19:09:29 -0700

    Update 20220802232508-demo-image.js

--- backend/db/seeders/20220802232508-demo-image.js
+++ backend/db/seeders/20220802232508-demo-image.js
@@ -6,8 +6,8 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 module.exports = {
-  options.tablename = 'Images'
   async up(queryInterface, Sequelize) {
+    options.tablename = 'Images'
     await queryInterface.bulkInsert(options, [
       {
         url: 'https://images.pexels.com/photos/439391/pexels-photo-439391.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
